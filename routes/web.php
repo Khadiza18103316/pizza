513418e4bf23e4f7426afd60f13936cd597cb817
Khadiza <khadizaakhi756@gmail.com>
2021-12-23 15:39:14 +0600

    admin orders dashboard

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,9 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::group(['middleware' =>'auth','admin'],function(){
+    Route::get('/', function () {
+        return view('pizza.create');
+    })->name('pizza.create');
 
 Route::get('/pizza', [App\Http\Controllers\PizzaController::class, 'index'])->name('pizza.index');
 Route::get('/pizza/create', [App\Http\Controllers\PizzaController::class, 'create'])->name('pizza.create');
@@ -29,5 +32,10 @@ Route::post('/pizza/store', [App\Http\Controllers\PizzaController::class, 'store
 Route::get('/pizza/{id}/edit', [App\Http\Controllers\PizzaController::class, 'edit'])->name('pizza.edit');
 Route::put('/pizza/{id}/update', [App\Http\Controllers\PizzaController::class, 'update'])->name('pizza.update');
 Route::delete('/pizza/{id}/delete', [App\Http\Controllers\PizzaController::class, 'destroy'])->name('pizza.destroy');
+
+//user order
+Route::get('/user/order', [App\Http\Controllers\UserOrderController::class, 'index'])->name('user.order');
+Route::post('/order/{id}/status', [App\Http\Controllers\UserOrderController::class, 'changeStatus'])->name('order.status');
+
 
 });
